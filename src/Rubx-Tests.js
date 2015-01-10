027cc770831ef30875195c5ab85da66cba17a02a
JohnnyT <ubergeek3141@gmail.com>
2015-01-10 15:17:48 -0700

    Adds Rubx and Rubx-Tests Amber packages

---
+++ src/Rubx-Tests.js
@@ -0,0 +1,8 @@
+define("amber-rubx/Rubx-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+$core.addPackage('Rubx-Tests');
+$core.packages["Rubx-Tests"].transport = {"type":"amd","amdNamespace":"amber-rubx"};
+
+$core.addClass('RubxTest', $globals.TestCase, [], 'Rubx-Tests');
+
+});
