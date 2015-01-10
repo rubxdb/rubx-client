c43883f49d45c5e30a01fb183922b73cbfadad05
JohnnyT <ubergeek3141@gmail.com>
2015-01-10 15:16:26 -0700

    :pencil: Adds Amber config

---
+++ config.js
@@ -0,0 +1,71 @@
+/* DO NOT EDIT! This file is generated. */
+
+var require;
+if (!require) require = {config: function (x) {require = x;}};
+require.config({
+  "paths": {
+    "helios/showdown": "bower_components/showdown/src/showdown",
+    "requireJS": "node_modules/requirejs/require",
+    "jquery": "bower_components/jquery/jquery",
+    "jquery-ui": "bower_components/jquery-ui/jquery-ui",
+    "codemirror": "bower_components/codemirror",
+    "bootstrap2.3.2": "bower_components/bootstrap2.3.2/bootstrap",
+    "mousetrap": "bower_components/mousetrap/mousetrap",
+    "jquery.blockUI": "bower_components/blockui/jquery.blockUI",
+    "require-css": "bower_components/require-css",
+    "amber_lib/es5-shim": "bower_components/es5-shim",
+    "helios": "bower_components/helios/src",
+    "helios/resources": "bower_components/helios/resources",
+    "helios/index": "bower_components/helios/index",
+    "amber-ide-starter-dialog": "bower_components/amber-ide-starter-dialog/lib/idestarter",
+    "amber-attic": "bower_components/amber-attic/src",
+    "amber-attic/resources": "bower_components/amber-attic/resources",
+    "amber": "bower_components/amber/support",
+    "amber_core": "bower_components/amber/src",
+    "amber-rubx": "src"
+  },
+  "rjs_comment1": "require.min should be removed",
+  "rjs_comment2": "it's backward compatibility hack if config finds",
+  "rjs_comment3": "support/requirejs before node_modules/requirejs;",
+  "rjs_comment4": "the former will go away in 0.14.0",
+  "shim": {
+    "jquery-ui": {
+      "deps": [
+        "jquery",
+        "jquery"
+      ]
+    },
+    "codemirror/lib/codemirror": {
+      "deps": [
+        "css!codemirror/lib/codemirror",
+        "css!codemirror/lib/codemirror"
+      ]
+    },
+    "bootstrap2.3.2/js/bootstrap": {
+      "deps": [
+        "jquery",
+        "css!bootstrap2.3.2/css/bootstrap"
+      ]
+    },
+    "amber-attic/IDE": {
+      "deps": [
+        "codemirror/lib/codemirror",
+        "codemirror/mode/smalltalk/smalltalk",
+        "codemirror/addon/hint/show-hint",
+        "css!codemirror/theme/ambiance",
+        "css!codemirror/lib/codemirror",
+        "css!codemirror/addon/hint/show-hint",
+        "jquery-ui",
+        "css!./resources/amber"
+      ]
+    },
+    "ensure-console": {
+      "exports": "console"
+    }
+  },
+  "map": {
+    "*": {
+      "css": "require-css/css"
+    }
+  }
+});
